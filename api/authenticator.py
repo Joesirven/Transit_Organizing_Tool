44e1895148bd2544388d833516971f6ca852961b
Jose <jose@sirven.xyz>
2024-02-12 01:21:41 -0500

    create fastapi authenticator

---
+++ api/authenticator.py
@@ -0,0 +1,28 @@
+import os
+from fastapi import Depends
+from jwtdown_fastapi.authentication import Authenticator
+from queries.users import UserRepository, UserOut, UserOutWithPassword
+
+
+class TSAAuthenticator(Authenticator):
+    async def get_account_data(
+        self,
+        email: str,
+        users: UserRepository,
+    ):
+        return users.get_one(email)
+
+    def get_account_getter(
+        self,
+        users: UserRepository = Depends(),
+    ):
+        return users
+
+    def get_hashed_password(self, user: UserOutWithPassword):
+        return user.hashed_password
+
+    def get_account_data_for_cookie(self, user: UserOut):
+        return user.email, UserOut(**user.dict())
+
+
+authenticator = TSAAuthenticator(os.environ["SIGNING_KEY"])
