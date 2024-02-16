aa999b56963d631c30af07a1e5705e1ac60c1ea4
Jose <jose@sirven.xyz>
2024-02-15 19:58:47 -0500

    install sql alchemy in venv, create crud for db

--- db/models.py
+++ db/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship
 
 from .database import Base
@@ -7,10 +7,16 @@ from .database import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(String, primary_key=True)
     email = Column(String, unique=True, index=True)
+    first_name = Column(String)
+    last_name = Column(String)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
+    email_verified = Column(Boolean, default=False)
+    profile_picture_url = Column(String)
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
 
     items = relationship("Item", back_populates="owner")
 
